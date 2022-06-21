b195fb279c197842997eac88cedcbf29046ed43d
Szymon Gamża <gamza.szymon@gmail.com>
2022-06-21 21:47:18 +0200

    First and propably last commit

---
+++ app/src/main/java/com/example/calculator/AboutActivity.java
@@ -0,0 +1,20 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setTitle("About");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
